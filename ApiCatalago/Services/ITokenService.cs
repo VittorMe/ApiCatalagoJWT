51be624386db027e40513139096ba193a9f070ef
vittor <vittordemelo@gmail.com>
2022-06-17 05:42:14 -0300

    JWT

--- ApiCatalago/Services/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApiCatalago.Models;
-
-namespace ApiCatalago.Services
-{
-    public interface ITokenService
-    {
-        string GerarToken(string key, string issuer, string audience, UserModel user);
-    }
-}
